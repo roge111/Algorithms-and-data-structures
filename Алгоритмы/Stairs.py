43b25aabb978762521e3e098b5ded3cd9ee7e413
Egor <64649951+roge111@users.noreply.github.com>
2025-04-14 00:15:16 +0300

    Update Stairs.py

--- Алгоритмы/Stairs.py
+++ Алгоритмы/Stairs.py
@@ -1,12 +1,4 @@
 def stairs(n):
-    # def f(x, y):
-    #     if x > y:
-    #         return 0
-    #     elif x == y:
-    #         return 1
-    #     else:
-    #         return f(x + 1, y) + f(x + 2, y)
-    # return f(0, n)
     dp = [0 for i in range(n + 1)]
     dp[0] = 1
     for i in range(n):
