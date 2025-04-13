1820ecdf9a5dd3e293e4aff94f1d378deea915ce
Egor <64649951+roge111@users.noreply.github.com>
2025-04-14 00:11:24 +0300

    Create BestTimeToBuyAndSellStock.py

---
+++ Алгоритмы/BestTimeToBuyAndSellStock.py
@@ -0,0 +1,10 @@
+def maxProfit(self, prices):
+      min_price = 10**4 + 1
+      max_profit = 0
+
+      for price in prices:
+          if price < min_price:
+              min_price = price
+          elif price - min_price > max_profit:
+              max_profit = price - min_price
+      return max_profit
